f945cb835c88d06fb1e881fc4d8fefdb3f6affb5
Liviu Preda <liviupreda@gmail.com>
2019-10-27 12:01:47 +0200

    other types of props

--- Hello.js
+++ Hello.js
@@ -1,9 +1,15 @@
 class Hello extends React.Component {
   render() {
+    const props = this.props;
+    let bangs = '!'.repeat(props.bangs);
     return (
-      <p>
-        Hi {this.props.to} from {this.props.from}
-      </p>
+      <div>
+        <p>
+          Hi {props.to} from {props.from}
+          {bangs}
+        </p>
+        <img src={props.img} />
+      </div>
     );
   }
 }
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to='Ringo' from='Paul' />
-        <Hello to='Cher' from='Sonny' />
+        <Hello
+          to='Ringo'
+          from='Paul'
+          bangs={50}
+          img='https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+        />
       </div>
     );
   }
