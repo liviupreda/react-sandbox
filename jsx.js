c5ff17549c93ed3221c19b5f7d09b52f7ca39497
Liviu Preda <liviupreda@gmail.com>
2019-10-27 10:43:15 +0200

    embed js in jsx

--- jsx.js
+++ jsx.js
@@ -1,9 +1,13 @@
+function getMood() {
+  const moods = ['angry', 'hungry', 'silly', 'quiet', 'paranoid'];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
 class JSXDemo extends React.Component {
   render() {
     return (
       <div>
-        <h1>My Image</h1>
-        <img src='https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' />
+        <h1>My current mood is: {getMood()}</h1>
       </div>
     );
   }
