08d5acaf78ec78605afa4dcf0b521cc233c83bba
Liviu Preda <liviupreda@gmail.com>
2019-10-31 11:16:01 +0200

    restart clicker exercise

--- state-demo/src/Clicker.js
+++ state-demo/src/Clicker.js
@@ -1,39 +1,25 @@
 import React, { Component } from 'react';
 
 class Clicker extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      clicked: false,
-      gameOver: false
+      num: 0
     };
   }
 
-  handleClick = e => {
-    this.setState({ clicked: true });
-  };
-
-  randomNumber = () => {
-    let rand = Math.floor(Math.random() * 10 + 1);
-    return rand;
+  randNum = () => {
+    let rand = Math.floor(Math.random() * 10) + 1;
+    this.setState({ num: rand });
   };
 
   render() {
     return (
       <div>
-        <h1>
-          {this.state.clicked
-            ? `Number is: ${this.randomNumber()}`
-            : 'Click ze button'}
-        </h1>
-        {!this.state.gameOver ? (
-          <button onClick={this.handleClick}>CLICK ME</button>
-        ) : (
-          <h1>You lucky dawg!</h1>
-        )}
+        <h1>Number is {this.state.num}</h1>
+        <button onClick={this.randNum}>Get Number</button>
       </div>
     );
   }
 }
-
 export default Clicker;
