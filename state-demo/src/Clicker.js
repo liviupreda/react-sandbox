717a974e08d95bbd5979201cb5e80ac7e9e212cc
Liviu Preda <liviupreda@gmail.com>
2019-10-31 11:33:14 +0200

    add isWinner state attribute

--- state-demo/src/Clicker.js
+++ state-demo/src/Clicker.js
@@ -4,12 +4,14 @@ class Clicker extends Component {
   constructor() {
     super();
     this.state = {
-      num: 0
+      num: 0,
+      isWinner: false
     };
   }
 
   randNum = () => {
     let rand = Math.floor(Math.random() * 10) + 1;
+    if (rand === 7) this.setState({ isWinner: true });
     this.setState({ num: rand });
   };
 
@@ -18,7 +20,7 @@ class Clicker extends Component {
       <div>
         <h1>Number is: {this.state.num}</h1>
 
-        {this.state.num === 7 ? (
+        {this.state.isWinner ? (
           <h2>You da man!!</h2>
         ) : (
           <button onClick={this.randNum}>Get Number</button>
