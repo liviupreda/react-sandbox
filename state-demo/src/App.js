c104412b149487f148c18178d7840faa1e3125c2
Liviu Preda <liviupreda@gmail.com>
2019-10-31 08:35:19 +0200

    set state

--- state-demo/src/App.js
+++ state-demo/src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from "react";
-import Game from "./Game";
-import Demo from "./Demo";
-import Button from "./Button";
-import BrokenClick from "./BrokenClick";
-import Rando from "./Rando";
-import Clicker from "./Clicker";
-import "./App.css";
+import React, { Component } from 'react';
+import Game from './Game';
+import Demo from './Demo';
+import Button from './Button';
+import BrokenClick from './BrokenClick';
+import Rando from './Rando';
+import Clicker from './Clicker';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Game />
-        <Demo />
-        <Button />
-        <Rando maxNum={10} />
-        <Clicker />
+        <Rando maxNum={7} />
       </div>
     );
   }
