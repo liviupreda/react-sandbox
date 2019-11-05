93e0a1710e7f6a82559ed811eab43a60cc03f2c0
Liviu Preda <liviupreda@gmail.com>
2019-11-05 15:25:20 +0200

    bind dispenseWisdom method in constructor

--- binding-demo/src/WiseSquareWithProps.js
+++ binding-demo/src/WiseSquareWithProps.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class WiseSquareWithProps extends Component {
   constructor(props) {
@@ -7,14 +7,14 @@ class WiseSquareWithProps extends Component {
   }
   static defaultProps = {
     messages: [
-      "A fool thinks himself to be wise, but a wise man knows himself to be a fool.",
-      "Educating the mind without educating the heart is no education at all.",
-      "Not everything that is faced can be changed, but nothing can be changed until it is faced."
+      'A fool thinks himself to be wise, but a wise man knows himself to be a fool.',
+      'Educating the mind without educating the heart is no education at all.',
+      'Not everything that is faced can be changed, but nothing can be changed until it is faced.'
     ]
   };
 
   dispenseWisdom() {
-    console.log("THIS IS...", this);
+    console.log('THIS IS...', this);
     let { messages } = this.props;
     let rIndex = Math.floor(Math.random() * messages.length);
     console.log(messages[rIndex]);
