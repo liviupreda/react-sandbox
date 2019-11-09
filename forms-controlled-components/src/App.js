b3b12ffed7f67c4365ade038f0b70a762c9d93d2
Liviu Preda <liviupreda@gmail.com>
2019-11-09 09:23:29 +0200

    form as controlled component

--- forms-controlled-components/src/App.js
+++ forms-controlled-components/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import Form from "./Form";
-import MultipleForm from "./MultipleForm";
-import "./App.css";
+import React, { Component } from 'react';
+import Form from './Form';
+import MultipleForm from './MultipleForm';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Form />
-        <MultipleForm />
+        {/* <MultipleForm /> */}
       </div>
     );
   }
--- forms-controlled-components/src/Form.js
+++ forms-controlled-components/src/Form.js
@@ -1,33 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Form extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: "" };
+    this.state = { username: '' };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange(evt) {
-    this.setState({ username: evt.target.value });
+
+  handleChange(e) {
+    this.setState({ username: e.target.value });
   }
-  handleSubmit(evt) {
-    evt.preventDefault();
-    alert(`You typed: ${this.state.username}`);
-    this.setState({ username: "" });
-  }
+
   render() {
     return (
       <div>
         <h1>Form Demo</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor='username'>Username</label>
+        <form>
           <input
             type='text'
-            id='username'
             value={this.state.username}
             onChange={this.handleChange}
           />
-          <button>Submit!</button>
         </form>
       </div>
     );
