71a057d1dd8c0761754fefc84b30764c483cff59
Liviu Preda <liviupreda@gmail.com>
2019-11-10 09:09:47 +0200

    shopping list initial commit

--- forms-shopping-list-demo/src/ShoppingList.js
+++ forms-shopping-list-demo/src/ShoppingList.js
@@ -1,30 +1,24 @@
-import React, { Component } from "react";
-import ShoppingListForm from "./ShoppingListForm";
-import uuid from "uuid/v4";
+import React, { Component } from 'react';
+import ShoppingListForm from './ShoppingListForm';
+import uuid from 'uuid/v4';
 
 class ShoppingList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       items: [
-        { name: "Milk", qty: "2 gallons", id: uuid() },
-        { name: "Bread", qty: "2 loaves", id: uuid() }
+        { name: 'Milk', qty: '2 gallons', id: uuid() },
+        { name: 'Bread', qty: '2 loaves', id: uuid() }
       ]
     };
-    this.addItem = this.addItem.bind(this);
   }
-  addItem(item) {
-    let newItem = { ...item, id: uuid() };
-    this.setState(state => ({
-      items: [...state.items, newItem]
-    }));
-  }
+
   renderItems() {
     return (
       <ul>
         {this.state.items.map(item => (
           <li key={item.id}>
-            {item.name}:{item.qty}
+            {item.name}: {item.qty}
           </li>
         ))}
       </ul>
@@ -35,7 +29,6 @@ class ShoppingList extends Component {
       <div>
         <h1>Shopping List</h1>
         {this.renderItems()}
-        <ShoppingListForm addItem={this.addItem} />
       </div>
     );
   }
